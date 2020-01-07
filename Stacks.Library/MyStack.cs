8abb40ab57bcc2954ef47e0b01d5b2297bc8e076
Rafael Delgado <rafael.delgado@greatcall.com>
2020-01-07 14:03:36 -0800

    Convert MyStack to a generic stack

--- Stacks.Library/MyStack.cs
+++ Stacks.Library/MyStack.cs
@@ -1,16 +1,16 @@
 using System;
 namespace Stacks.Library
 {
-    public class MyStack
+    public class MyStack<T>
     {
-        private object [] _items;
+        private T [] _items;
         private int _currentIndex;
         public MyStack(int capacity)
         {
-            _items = new object[capacity];
+            _items = new T[capacity];
             _currentIndex = 0;
         }
-        public void Push(object value)
+        public void Push(T value)
         {
             if (_currentIndex == _items.Length)
                 throw new Exception();
@@ -19,10 +19,10 @@ namespace Stacks.Library
 
             _items[_currentIndex++] = value;
         }
-        public object Pop()
+        public T Pop()
         {
             if (_currentIndex == 0)
-                return null;
+                return default(T);
             return _items[--_currentIndex];
         }
     }
