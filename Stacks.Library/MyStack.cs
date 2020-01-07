ffe2d743e0db665ade147cdcc129265cc1cbb25c
Rafael Delgado <rafael.delgado@greatcall.com>
2020-01-07 12:32:18 -0800

    Update implementation to use an array

--- Stacks.Library/MyStack.cs
+++ Stacks.Library/MyStack.cs
@@ -3,18 +3,20 @@ namespace Stacks.Library
 {
     public class MyStack
     {
-        private object _item;
+        private object [] _items;
+        private int _currentIndex;
         public MyStack(int capacity)
         {
-            
+            _items = new object[capacity];
+            _currentIndex = 0;
         }
         public void Push(object value)
         {
-            _item = value;
+            _items[_currentIndex++] = value;
         }
         public object Pop()
         {
-            return _item;
+            return _items[--_currentIndex];
         }
     }
 }
