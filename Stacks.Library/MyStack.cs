369416a08fc51b081a9db047941f8084deac04b1
rafael@delgado.com <rafael@delgado.com>
2020-01-06 22:18:04 -0800

    Add MyStack class

---
+++ Stacks.Library/MyStack.cs
@@ -0,0 +1,19 @@
+
+namespace Stacks.Library
+{
+    public class MyStack
+    {
+        public MyStack(int capacity)
+        {
+            
+        }
+        public void Push(string value)
+        {
+
+        }
+        public string Pop()
+        {
+            return string.Empty;
+        }
+    }
+}
