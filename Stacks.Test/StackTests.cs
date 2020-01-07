8f91c729e14125c8801b950984175faaec7018fb
rafael@delgado.com <rafael@delgado.com>
2020-01-06 22:17:46 -0800

    Add CanPopOffItem

--- Stacks.Test/StackTests.cs
+++ Stacks.Test/StackTests.cs
@@ -1,10 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stacks.Library;
 
 namespace Stacks.Test
 {
     [TestClass]
     public class StackTests
     {
-
+        [TestMethod]
+        public void CanPopOffItem()
+        {
+            var stack = new MyStack(100);
+            stack.Push("foo");
+            Assert.AreEqual("foo", stack.Pop());
+        }
     }
 }
